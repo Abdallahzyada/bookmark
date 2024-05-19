50240ed76457a5465c1dca4f089d4fbc2ec443c3
unknown <abdallahzyada25@gmail.com>
2024-05-19 15:59:22 +0300

    Adding user actions to the activity stream

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from actions.utils import create_action
 
 def user_login(request):
     if request.method == 'POST':
@@ -43,15 +44,12 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set the chosen password
             new_user.set_password(
                 user_form.cleaned_data['password'])
-            # Save the User object
             new_user.save()
-            # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(request.user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -120,6 +118,7 @@ def user_follow(request):
                     user_from = request.user,
                     user_to = user
                 )
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(
                     user_from = request.user,
